32a4cd141b1b527efd48315b17d4dcfdeb26dfac
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-22 09:27:53 -0500

    feat: demo-spring-ecommerce, proyecto demo de ecommerce creado

---
+++ src/main/java/pe/popehiflo/demo/ecommerce/DemoSpringEcommerceApplication.java
@@ -0,0 +1,13 @@
+package pe.popehiflo.demo.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringEcommerceApplication.class, args);
+	}
+
+}
