837a8cc49534b4916f86bbc2cd864ba0d59e4097
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-22 09:31:41 -0500

    feat: DemoSpringEcommerceApplication, excluir momentaneamente config a DB

--- src/main/java/pe/popehiflo/demo/ecommerce/DemoSpringEcommerceApplication.java
+++ src/main/java/pe/popehiflo/demo/ecommerce/DemoSpringEcommerceApplication.java
@@ -2,8 +2,9 @@ package pe.popehiflo.demo.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoSpringEcommerceApplication {
 
 	public static void main(String[] args) {
