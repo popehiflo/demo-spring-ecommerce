dbd34fe539cda0553ac5bb1fdaad0a06f3c2d48e
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-24 00:16:54 -0500

    feat: Entity DetalleOrden, added getters setters anotaciones y relaciones

--- src/main/java/pe/popehiflo/demo/ecommerce/model/DetalleOrden.java
+++ src/main/java/pe/popehiflo/demo/ecommerce/model/DetalleOrden.java
@@ -1,12 +1,29 @@
 package pe.popehiflo.demo.ecommerce.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "detalles_ordenes")
 public class DetalleOrden {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private double cantidad;
 	private double precio;
 	private double total;
+	
+	@OneToOne
+	private Orden orden;
+	
+	@OneToOne
+	private Producto producto;
 	
 	public DetalleOrden() {
 		super();
@@ -51,6 +68,23 @@ public class DetalleOrden {
 	public void setTotal(double total) {
 		this.total = total;
 	}
+	
+	public Orden getOrden() {
+		return orden;
+	}
+
+	public void setOrden(Orden orden) {
+		this.orden = orden;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
 	@Override
 	public String toString() {
 		return "DetalleOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio
