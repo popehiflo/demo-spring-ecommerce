727f199d73c3b57f1f764ab15f9dd2a3bade16b2
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-24 00:34:14 -0500

    feat: DemoSpringEcommerApplication, borra exclusion a la DB

--- src/main/java/pe/popehiflo/demo/ecommerce/DemoSpringEcommerceApplication.java
+++ src/main/java/pe/popehiflo/demo/ecommerce/DemoSpringEcommerceApplication.java
@@ -2,9 +2,8 @@ package pe.popehiflo.demo.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class DemoSpringEcommerceApplication {
 
 	public static void main(String[] args) {
